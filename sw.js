3ac5e9bcafec5917f84c839259a011d73bbe40e0
natan-bakshi <natib8000@gmail.com>
2026-08-24 17:02:43 +0000

    Refresh branded landing page design

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bar-mitzva-calculator-v1';
+const CACHE_NAME = 'bar-mitzva-calculator-v2';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
